23f84180131582d68c586e6ca63cd9e50b381924
Naufal Hanif Cipta Darmara <108689285+upilupal@users.noreply.github.com>
2024-03-21 12:03:20 +0700

    feat: add layouting on homepage (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,45 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import {
+  Bell,
+  Home as HomeIcon,
+  MessageSquareText,
+  UserRound,
+} from "lucide-react";
+import HeaderTweet from "./components/HeaderTweet";
+import CardWhoToFollow from "./components/CardWhoToFollow";
 export default function Home() {
   return (
-    <>
-    </>
+    <main className="container mx-auto px-4">
+      <section className=" grid grid-cols-5 pt-4">
+        {/* SIDEBAR */}
+        <div className="flex flex-col gap-4">
+          <Button variant="ghost" className="justify-normal">
+            <HomeIcon className="mr-2 h-4 w-4" /> Home
+          </Button>
+          <Button variant="ghost" className="justify-normal">
+            <Bell className="mr-2 h-4 w-4" /> Notification
+          </Button>
+          <Button variant="ghost" className="justify-normal">
+            <MessageSquareText className="mr-2 h-4 w-4" /> Message
+          </Button>
+          <Button variant="ghost" className="justify-normal">
+            <UserRound className="mr-2 h-4 w-4" /> Profile
+          </Button>
+        </div>
+        {/* END SIDEBAR */}
+
+        {/* MAIN SECTION */}
+        <div className="col-span-3">
+          <HeaderTweet />
+        </div>
+        {/* MAIN SECTION END */}
+
+        {/* WHO TO FOLLOW */}
+        <div>
+          <CardWhoToFollow/>
+        </div>
+        {/* WHO TO FOLLOW END */}
+      </section>
+    </main>
   );
 }
